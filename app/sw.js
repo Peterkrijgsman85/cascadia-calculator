e7f7fdac99f01a47fcb016ae46b335e9f5a4e271
Peter Krijgsman <36139454+Peterkrijgsman85@users.noreply.github.com>
2026-06-04 21:09:23 +0200

    Add service worker for caching assets

---
+++ app/sw.js
@@ -0,0 +1,21 @@
+const CACHE = "cascadia-v2";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
